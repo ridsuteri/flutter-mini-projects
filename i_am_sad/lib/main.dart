3964469eb14d836e379a7aeee5ba42bc1d2dc19c
Riddhi Suteri <ridsuteri@gmail.com>
2022-07-16 08:37:54 +0530

    add I am sad app

---
+++ i_am_sad/lib/main.dart
@@ -0,0 +1,18 @@
+import 'package:flutter/material.dart';
+
+void main() {
+  runApp(
+    MaterialApp(
+      home: Scaffold(
+        appBar: AppBar(
+          title: Text('I am Sad'),
+          backgroundColor: Colors.teal,
+        ),
+        body: Center(
+          child: Text(
+              'Itna🌚 chubne🌵 laga hu sabko👥 chura🗡️ to nahi hu jaani👩💟 jitna batate👄🗣️ ho mere🦍 bare me🤡 utna bura👹 to nahi hu😟🥀'),
+        ),
+      ),
+    ),
+  );
+}
